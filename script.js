976485abd75ef8e2d9dfaa431d5ba511b436cafd
AnezaCode <156332330+anezacode@users.noreply.github.com>
2024-07-29 22:07:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSetting() {
+    const set = document.getElementById("setting");
+    if (set.style.display === "block") {
+        set.style.display = "none";
+    } else {
+        set.style.display = "block";
+    }
+}
