1b013207b8a96b5edb6a81fd7f05cdd3d723e0a1
Your Name <you@example.com>
2024-09-02 20:09:22 +0700

    modified:   engine.js 	modified:   style.css

--- engine.js
+++ engine.js
@@ -1,15 +1,12 @@
 function sumberDoksli() {
     const doksli = document.getElementById('bar-search').value.trim().toLowerCase();
     const keyword = [
-        "si imut dari laut jawa",
-        "kapal karam"
+        "si imut dari laut jawa"
     ];
     const dok1 = document.getElementById('dok1');
 
     if (doksli.length > 0 && keyword[0].includes(doksli)) {
         dok1.style.display = 'block';
-    } else {
-        dok1.style.display = 'none';
     }
  
     return false;
